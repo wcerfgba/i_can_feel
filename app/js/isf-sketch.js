dd6f2fcdab305e9ed926e150a7d446e1dbec8d5d
John L. Preston <wcerfgba@riseup.net>
2018-04-15 00:37:56 +0100

    mo

--- app/js/isf-sketch.js
+++ app/js/isf-sketch.js
@@ -3,50 +3,29 @@ module.exports = `
 /*{
   "CREDIT": "",
   "DESCRIPTION": "",
-  "CATEGORIES": [
-    "generator"
-  ],
+  "CATEGORIES": [],
   "INPUTS": []
 }*/
 
 #ifdef GL_ES
 precision highp float;
 #endif
- 
-#define iterations 7
-#define volsteps 7
-
-float field(in vec3 p) 
-{
-	float strength = 7. + .03 * log(1.e-6 + fract(sin(TIME) * 4373.11));
-	float accum = 0.;
-	float prev = 0.;
-	float tw = 0.;
-	for (int i = 0; i < 7; ++i) 
-	{
-		float mag = dot(p, p);
-		p = abs(p) / mag + vec3(-.5, -.4, -1.5);
-		float w = exp(-float(i) / 7.);
-		accum += w * exp(-strength * pow(abs(mag - prev), 2.3));
-		tw += w;
-		prev = mag;
-	}
-	return max(0., 5. * accum / tw - .7);
-}
 
 void main() {
-	float res = 128.;
-  float depth = 2.;
-  float scale = pow(2., 14.);
+  float phi = 1.6180339887;
 
-  float t = TIME;
+  float X = 64.;
+  float T = 10.;
+  
+  float t = TIME / T;
+  float i = ((floor(X * isf_FragNormCoord.x) +
+              floor(X * isf_FragNormCoord.y) * X)) / ((X * X) + X);
 
-  vec2 uv = floor(isf_FragNormCoord * res);
-  float n = pow(depth, 1. + (uv.x + (res * uv.y)) / scale);
+  float m = i;
 
-  float v = mod(scale * depth * t / n, depth) / (depth - 1.);
+  float c = mod(t, m);
 
-  vec4 col = vec4(v, v, v, 1.);
+  vec4 col = vec4(c, c, c, 1.);
   gl_FragColor = col;
 }
 
