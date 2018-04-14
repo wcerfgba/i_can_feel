1bb5f9bf7a21daf12f03284fde9aabc9f5c36468
John L. Preston <wcerfgba@riseup.net>
2018-04-15 00:38:27 +0100

    n mo

--- app/js/isf-sketch.js
+++ app/js/isf-sketch.js
@@ -12,21 +12,23 @@ precision highp float;
 #endif
 
 void main() {
-  float phi = 1.6180339887;
-
-  float X = 64.;
-  float T = 10.;
-  
-  float t = TIME / T;
-  float i = ((floor(X * isf_FragNormCoord.x) +
+  float X = 24.;
+  float G = 5.75;
+  float N = (X * X) / G;
+  float t = 991677934871. * TIME;
+  float x = ((floor(X * isf_FragNormCoord.x) +
               floor(X * isf_FragNormCoord.y) * X)) / ((X * X) + X);
+  float n = floor(x * N) / N;
+  float i = (1. / G) * mod(x * X * X, G);
 
-  float m = i;
+  float gv = (1. / n) * mod(t, n);
+  float cv = (1. / i) * mod(gv, i);
 
-  float c = mod(t, m);
-
-  vec4 col = vec4(c, c, c, 1.);
+  vec4 col = vec4(cv, cv, cv, 1.);
   gl_FragColor = col;
 }
 
+// Some nice time formulae:
+//
+//  float t = 10308848.7745 * TIME;  //  
 `
