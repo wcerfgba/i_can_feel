85b3792c4c5ec4af5705586b6c9ffb505840ae96
John L. Preston <wcerfgba@riseup.net>
2018-04-15 12:08:22 +0100

    reorganise slightly

--- app/js/index.js
+++ app/js/index.js
@@ -1,35 +1,49 @@
 const ISFRenderer = require('interactive-shader-format').Renderer
-const ControlKit = require('controlkit')
 
 const canvas = document.querySelector('#isf-canvas')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-const gl = canvas.getContext("webgl");
-const renderer = new ISFRenderer(gl);
+const gl = canvas.getContext("webgl")
+const renderer = new ISFRenderer(gl)
 
-const isfFragment = require('./isf-sketch')
-renderer.loadSource(isfFragment);
-// renderer.setValue("dotSize", .05)
+const isfFragment = `
+/*{
+  "CREDIT": "",
+  "DESCRIPTION": "",
+  "CATEGORIES": [],
+  "INPUTS": []
+}*/
+
+#ifdef GL_ES
+precision highp float;
+#endif
+
+void main() {
+  float X = 24.;
+  float G = 5.75;
+  float N = (X * X) / G;
+  float t = 991677934871. * TIME;
+  float x = ((floor(X * isf_FragNormCoord.x) +
+              floor(X * isf_FragNormCoord.y) * X)) / ((X * X) + X);
+  float n = floor(x * N) / N;
+  float i = (1. / G) * mod(x * X * X, G);
+
+  float gv = (1. / n) * mod(t, n);
+  float cv = (1. / i) * mod(gv, i);
+
+  vec4 col = vec4(cv, cv, cv, 1.);
+  gl_FragColor = col;
+}
+
+// Some nice time formulae:
+//
+//  float t = 10308848.7745 * TIME;  //  
+`
+renderer.loadSource(isfFragment)
 
 const animate = () => {
   requestAnimationFrame(animate)
-  renderer.draw(canvas);
+  renderer.draw(canvas)
 }
 animate()
-
-// ControlKit.setup()
-// const controlKitPanel = ControlKit.addPanel()
-// const params = { 
-//   dotSize: { value: 0.75, range: [0.5, 1.0] }
-// }
-// controlKitPanel.addSlider(
-//   params.dotSize, // Object to update
-//   'value', 'range', // key for value and range
-//   { // options
-//     label: 'morph',
-//     onChange: function(v) {
-//       renderer.setValue("morph", params.dotSize.value)
-//     }
-//   }
-// )
